ca42c39a13c7ef7c4b897dafdfa816c421480c99
ovoYJ <do_ob199@naver.com>
2021-11-19 17:25:42 +0900

    [GSTS] 대표번호 관리, 비상멘트 관리 삭제 안됨 수정

--- REAL-GSHome/src/main/webapp/assets/js/view/js_create/bakeGR/ivrManagement/ivrGRDnis.js
+++ REAL-GSHome/src/main/webapp/assets/js/view/js_create/bakeGR/ivrManagement/ivrGRDnis.js
@@ -354,7 +354,7 @@ fnObj.gridView01 = axboot.viewExtend(axboot.gridView, {
 
         if (_type == "modified" || _type == "deleted") {
             list = ax5.util.filter(_list, function () {
-                return this.ani;
+                return this.dnis;
             });
         } else {
             list = _list;
